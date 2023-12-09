73db59e1a5cadea0461ced32f52a13128024f387
Matthew Dirks <majadirks@gmail.com>
2023-12-08 17:59:10 -0800

    misc

--- LatinDwarsliggerLogic/HtmlFormatter.cs
+++ LatinDwarsliggerLogic/HtmlFormatter.cs
@@ -2,14 +2,6 @@
 {
     public class HtmlFormatter
     {
-        public static IEnumerable<string> ReadFile(string filename)
-        {
-            using StreamReader reader = new(filename);
-            string? line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                yield return line;
-            }
-        }
+        
     }
 }
---
+++ LatinDwarsliggerTest/HtmlFormaterTests.cs
@@ -0,0 +1,22 @@
+﻿using LatinDwarsliggerLogic;
+
+namespace LatinDwarsliggerTest
+{
+    [TestClass]
+    public class HtmlFormaterTests
+    {
+        [TestMethod]
+        public void Test_ReadFile()
+        {
+            // Arrange
+            string path = @"resources\aen1.html";
+            //Act
+            string[] lines = File.ReadAllLines(path);
+
+            // Assert
+            Assert.IsNotNull(lines);
+            Assert.IsTrue(lines.Any());
+            Assert.IsTrue(lines.Any(str => str.Contains("rma virumque canō, Trōiae quī prīmus ab ōrīs")));
+        }
+    }
+}
