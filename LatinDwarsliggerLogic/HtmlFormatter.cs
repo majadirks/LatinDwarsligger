98b532e0fde343bfb824cb0b48ef8b185884aafd
Matthew Dirks <majadirks@gmail.com>
2023-12-08 18:16:04 -0800

    Write test for stripping line numbers

--- LatinDwarsliggerLogic/HtmlFormatter.cs
+++ LatinDwarsliggerLogic/HtmlFormatter.cs
@@ -1,7 +1,24 @@
 ﻿namespace LatinDwarsliggerLogic
 {
-    public class HtmlFormatter
+    public static class HtmlFormatter
     {
-        
+        public static IEnumerable<string> StripLineNumbers(string[] verses)
+        {
+            return verses
+                .Select(verse => verse.Replace("&nbsp;", ""))
+                .Select(verse => DeleteSpanTags(verse));
+        }
+
+        /// <summary>
+        /// Find "<span", delete everything through subsequent </span>
+        /// </summary>
+        public static string DeleteSpanTags(string line)
+        {
+            int start = line.IndexOf("<span");
+            if (start == -1) return line; // no span to delete
+            int end = line.IndexOf("</span>");
+            if (end == -1) return line; // badly formed line; ignore
+            return line.Substring(0, start) + line.Substring(end + 7);
+        }
     }
 }
--- LatinDwarsliggerTest/HtmlFormaterTests.cs
+++ LatinDwarsliggerTest/HtmlFormaterTests.cs
@@ -6,17 +6,46 @@ namespace LatinDwarsliggerTest
     public class HtmlFormaterTests
     {
         [TestMethod]
-        public void Test_ReadFile()
+        public void StripLineNumbers_Test()
         {
             // Arrange
-            string path = @"resources\aen1.html";
-            //Act
-            string[] lines = File.ReadAllLines(path);
+            string[] lines =
+                [
+                    @"</p><p>",
+                    @"Arma virumque canō, Trōiae quī prīmus ab ōrīs<br>",
+                    @"Ītaliam, fātō profugus, Lāvīniaque vēnit<br>",
+                    @"lītora, multum ille et terrīs iactātus et altō<br>",
+                    @"vī superum saevae memorem Iūnōnis ob īram;<br>",
+                    @"multa quoque et bellō passus, dum conderet ",
+                    @"urbem,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style=""font-size: 80%;"">5</span><br>",
+                    @"inferretque deōs Latiō, genus unde Latīnum,<br>",
+                    @"Albānīque patrēs, atque altae moenia Rōmae.",
+                    @"</p>"
+                ];
+
+            string[] expected =
+            {
+                    @"</p><p>",
+                    @"Arma virumque canō, Trōiae quī prīmus ab ōrīs<br>",
+                    @"Ītaliam, fātō profugus, Lāvīniaque vēnit<br>",
+                    @"lītora, multum ille et terrīs iactātus et altō<br>",
+                    @"vī superum saevae memorem Iūnōnis ob īram;<br>",
+                    @"multa quoque et bellō passus, dum conderet ",
+                    @"urbem,<br>",
+                    @"inferretque deōs Latiō, genus unde Latīnum,<br>",
+                    @"Albānīque patrēs, atque altae moenia Rōmae.",
+                    @"</p>"
+            };
+
+            // Act
+            string[] actual = HtmlFormatter.StripLineNumbers(lines).ToArray();
 
             // Assert
-            Assert.IsNotNull(lines);
-            Assert.IsTrue(lines.Any());
-            Assert.IsTrue(lines.Any(str => str.Contains("Arma virumque canō, Trōiae quī prīmus ab ōrīs")));
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
         }
     }
 }
