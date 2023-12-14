385c59f6a33d620d612aaca28ac04801e770c8d8
Matthew Dirks <majadirks@gmail.com>
2023-12-13 22:05:56 -0800

    convert PaperSheet to record

--- LatinDwarsliggerLogic/PaperSheet.cs
+++ LatinDwarsliggerLogic/PaperSheet.cs
@@ -12,18 +12,4 @@
 /// ------	 -------
 
 /// </summary>
-public sealed class PaperSheet
-{
-    public PaperSheet(HalfSide sideA, HalfSide? sideB, HalfSide? sideC, HalfSide? sideD)
-    {
-        SideA = sideA;
-        SideB = sideB;
-        SideC = sideC;
-        SideD = sideD;
-    }
-
-    public HalfSide SideA { get; init; }
-    public HalfSide? SideB { get; init; }
-    public HalfSide? SideC { get; init; }
-    public HalfSide? SideD { get; init; }
-}
+public record PaperSheet(HalfSide SideA, HalfSide? SideB, HalfSide? SideC, HalfSide? SideD);
