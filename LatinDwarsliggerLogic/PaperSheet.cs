65067fcd640079dd2feb5e452bbaf375080ee52b
Matthew Dirks <majadirks@gmail.com>
2023-12-13 22:05:15 -0800

    PaperSheet not IDisposable

--- LatinDwarsliggerLogic/PaperSheet.cs
+++ LatinDwarsliggerLogic/PaperSheet.cs
@@ -1,51 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LatinDwarsliggerLogic;
 
-namespace LatinDwarsliggerLogic
+/// <summary>
+/// Represents a two-sided piece of paper to be printed and folded:
+/// 
+/// Consecutive pages A, B, C, D should come out looking like
+
+/// -------  -------    
+/// |  B  |	 | ∀  |
+/// |-----|	 |-----|
+/// |  C  |	 | (|  | (upside-down D)
+/// ------	 -------
+
+/// </summary>
+public sealed class PaperSheet
 {
-    /// <summary>
-    /// Represents a two-sided piece of paper to be printed and folded:
-    /// 
-    /// Consecutive pages A, B, C, D should come out looking like
-
-    /// -------  -------    
-    /// |  B  |	 | ∀  |
-    /// |-----|	 |-----|
-    /// |  C  |	 | (|  | (upside-down D)
-    /// ------	 -------
-
-    /// </summary>
-    public sealed class PaperSheet : IDisposable
+    public PaperSheet(HalfSide sideA, HalfSide? sideB, HalfSide? sideC, HalfSide? sideD)
     {
-        public PaperSheet(HalfSide sideA, HalfSide? sideB, HalfSide? sideC, HalfSide? sideD)
-        {
-            SideA = sideA;
-            SideB = sideB;
-            SideC = sideC;
-            SideD = sideD;
-            disposed = false;
-        }
-
-        public HalfSide SideA { get; init; }
-        public HalfSide? SideB { get; init; }
-        public HalfSide? SideC { get; init; }
-        public HalfSide? SideD { get; init; }
-        private bool disposed;
-
-        public void Dispose()
-        {
-            if (disposed) return;
-            SideA?.Dispose();
-            SideB?.Dispose();
-            SideC?.Dispose();
-            SideD?.Dispose();
-            GC.SuppressFinalize(this))
-            disposed = true;
-        }
-
-        ~PaperSheet() => Dispose();
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+        SideD = sideD;
     }
+
+    public HalfSide SideA { get; init; }
+    public HalfSide? SideB { get; init; }
+    public HalfSide? SideC { get; init; }
+    public HalfSide? SideD { get; init; }
 }
