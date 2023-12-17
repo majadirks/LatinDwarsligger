ff5dc0cd29c24e603e5f4f8ff5c244752859d75f
Matthew Dirks <majadirks@gmail.com>
2023-12-16 20:57:28 -0800

    comments

--- LatinDwarsliggerTest/Arranger_Tests.cs
+++ LatinDwarsliggerTest/Arranger_Tests.cs
@@ -78,7 +78,7 @@ public class Arranger_Tests
     [TestMethod]
     public void BreakLongWordAtChar()
     {
-        // Arrange
+        // Arrange: A few words with spaces, plus a really long word with no spaces
         string longString = "Quae res in civitate " + "duae plurimum possunt, eae contra nos ambae faciunt in hoc tempore, summa gratia et eloquentia; quarum alterum, C. Aquili, vereor, alteram metuo. Eloquentia Q. Hortensi ne me in dicendo impediat, non nihil commoveor, gratia Sex. Naevi ne P. Quinctio noceat, id vero non mediocriter pertimesco. Neque hoc tanto opere querendum videretur, haec summa in illis esse, si in nobis essent saltem mediocria; verum ita se res habet, ut ego, qui neque usu satis et ingenio parum possum, cum patrono disertissimo comparer, P. Quinctius, cui tenues opes, nullae facultates, exiguae amicorum copiae sunt, cum adversario gratiosissimo contendat.".Replace(" ", "");
         Paragraph paragraph = new Paragraph(new List<string> { longString });
         Arranger arr = Arranger.Default;
@@ -86,7 +86,7 @@ public class Arranger_Tests
         // Act
         var columns = arr.ArrangeParagraphsIntoColumns(new List<Paragraph> { paragraph } );
 
-        // Assert
+        // Assert : the first few words are on their own line, and then the remaining "long word" is split into several lines
         var contents = columns.Single().Contents;
         Assert.AreEqual("Quae res in civitate",contents.First().Trim());
         Assert.IsTrue(contents.Skip(1).First().StartsWith("duaeplurimumpossunt"));
